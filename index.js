8706aef43bf8e75bb2952f86f2eadc71d28f4e29
litoon000 <kirbydujojo29@gmail.com>
2026-05-23 13:57:24 +0200

    fix dotenv override

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+require('dotenv').config({ override: false });
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 
